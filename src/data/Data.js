5a6102bd6757b13853a2469cc9abf8bb55d3a830
DurgaKrishdp <durgakrish3553@gmail.com>
2021-11-18 18:57:16 +0530

    first commit

---
+++ src/data/Data.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+        "id": 1,
+        "title": "Smart Watch",
+        "cate": "Watch",
+        "price": 5000,
+        "image": "img/product-1.jpg",
+        "amount": 1
+    },
+    {   
+        "id": 2,
+        "title": "Bag",
+        "cate": "Bags",
+        "price": 1500,
+        "image": "img/product-2.jpg",
+        "amount": 1
+    },
+    {
+        "id": 3,
+        "title": "Diamond jewelers",
+        "cate": "jewelers",
+        "price": 100000,
+        "image": "img/product-3.jpg",
+        "amount": 1
+    },
+    {
+        "id": 4,
+        "title": "Nike Shoes",
+        "cate": "Shoes",
+        "price": 2000,
+        "image": "img/product-4.jpg",
+        "amount": 1
+    },
+    {
+        "id": 5,
+        "title": "Wallet",
+        "cate": "Men Accessories",
+        "price": 500,
+        "image": "img/product-5.jpg",
+        "amount": 1
+    },
+    {
+        "id": 6,
+        "title": "Hoodie",
+        "cate": "Dress",
+        "price": 1000,
+        "image": "img/product-6.jpg",
+        "amount": 1
+    },
+    {
+        "id": 7,
+        "title": "Women Bag Collection",
+        "cate": "Jwellry",
+        "price": 10000,
+        "image": "img/product-7.jpg",
+        "amount": 1
+    },
+    {
+        "id": 8,
+        "title": "Ear Buds",
+        "cate": "Accessories",
+        "price": 10000,
+        "image": "img/product-8.jpg",
+        "amount": 1 
+    },
+    {
+        "id": 9,
+        "title": "Camera",
+        "cate": "Accessories",
+        "price": 3000,
+        "image": "img/product-9.jpg",
+        "amount": 1
+    }
+]
+
+export default products;
